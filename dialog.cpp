d4a102252acde945fbebb2bfda81d745cf24c014
igntnk <ighnatenko@vk.com>
2023-04-25 20:34:01 +0300

    simple design
    the first design of the employee system app

---
+++ dialog.cpp
@@ -0,0 +1,17 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+#include "mainwindow.h"
+#include <QPixmap>
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
---
+++ mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "dialog.h"
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    Dialog window;
+    window.setModal(true);
+    window.exec();
+    close();
+}
+
